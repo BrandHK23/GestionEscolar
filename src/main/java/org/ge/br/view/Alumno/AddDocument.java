a8afd47475b76de66262d769ba110e13fb6090bc
Brandon Hinojosa <brandHK23@hotmail.com>
2023-07-04 14:22:24 -0600

    AddDocument update

--- src/main/java/org/ge/br/view/Alumno/AddDocument.java
+++ src/main/java/org/ge/br/view/Alumno/AddDocument.java
@@ -1,4 +1,5 @@
 package org.ge.br.view.Alumno;
+
 import org.ge.br.dao.DocAlumnoDao;
 import org.ge.br.model.Alumno;
 import org.ge.br.model.DocAlumno;
@@ -7,12 +8,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddDocument extends JFrame {
     private Alumno alumnoSeleccionado;
-    private JTextField[] filePathFields;
+    private JLabel[] filePathLabels;
+    private JButton[] openDirButtons;
 
     public AddDocument(Alumno alumno) {
         alumnoSeleccionado = alumno;
@@ -25,15 +29,19 @@ public class AddDocument extends JFrame {
         String[] nombresDocumentos = {"Titulo", "Cédula Profesional", "Acta de nacimiento", "Fotografias", "CURP", "Comprobante de domicilio", "Carta de intencion", "INE"};
 
         // Crear panel para mostrar los campos de ruta de los documentos
-        JPanel panelDocumentos = new JPanel(new GridLayout(nombresDocumentos.length, 2));
+        JPanel panelDocumentos = new JPanel(new GridLayout(nombresDocumentos.length, 4, 10, 0));
 
         // Crear campos de texto para las rutas de los documentos
-        filePathFields = new JTextField[nombresDocumentos.length];
+        filePathLabels = new JLabel[nombresDocumentos.length];
+        openDirButtons = new JButton[nombresDocumentos.length];
 
         for (int i = 0; i < nombresDocumentos.length; i++) {
             JLabel lblDocumento = new JLabel(nombresDocumentos[i]);
-            JTextField txtRuta = new JTextField();
+            JLabel lblRuta = new JLabel();
+            lblRuta.setPreferredSize(new Dimension(200, lblRuta.getPreferredSize().height));
             JButton btnSeleccionar = new JButton("Agregar");
+            JButton btnOpenDir = new JButton("Abrir");
+            btnOpenDir.setEnabled(false);
 
             // Configurar acción del botón "Seleccionar"
             final int index = i;
@@ -44,18 +52,30 @@ public class AddDocument extends JFrame {
                     int option = fileChooser.showOpenDialog(AddDocument.this);
                     if (option == JFileChooser.APPROVE_OPTION) {
                         String filePath = fileChooser.getSelectedFile().getAbsolutePath();
-                        txtRuta.setText(filePath);
+                        lblRuta.setText(filePath);
+                        btnOpenDir.setEnabled(true);
                     }
+                }
+            });
+
+            // Configurar acción del botón "Abrir Directorio"
+            btnOpenDir.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String rutaDocumento = lblRuta.getText();
+                    abrirDirectorio(rutaDocumento);
                 }
             });
 
             // Agregar los componentes al panel
             panelDocumentos.add(lblDocumento);
-            panelDocumentos.add(txtRuta);
+            panelDocumentos.add(lblRuta);
             panelDocumentos.add(btnSeleccionar);
+            panelDocumentos.add(btnOpenDir);
 
-            // Guardar referencia al campo de texto en el array
-            filePathFields[i] = txtRuta;
+            // Guardar referencia al label y al botón en los arrays
+            filePathLabels[i] = lblRuta;
+            openDirButtons[i] = btnOpenDir;
         }
 
         // Obtener las rutas de los documentos existentes para el alumno seleccionado
@@ -70,8 +90,8 @@ public class AddDocument extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 // Obtener las rutas de los documentos
                 List<String> rutasDocumentos = new ArrayList<>();
-                for (JTextField filePathField : filePathFields) {
-                    String rutaDocumento = filePathField.getText();
+                for (JLabel filePathLabel : filePathLabels) {
+                    String rutaDocumento = filePathLabel.getText();
                     rutasDocumentos.add(rutaDocumento);
                 }
 
@@ -110,11 +130,27 @@ public class AddDocument extends JFrame {
 
         for (DocAlumno documento : documentos) {
             int numDoc = documento.getNum_doc();
-            if (numDoc > 0 && numDoc <= filePathFields.length) {
-                JTextField textField = filePathFields[numDoc - 1];
-                textField.setText(documento.getRuta_doc());
+            if (numDoc > 0 && numDoc <= filePathLabels.length) {
+                JLabel label = filePathLabels[numDoc - 1];
+                label.setText(documento.getRuta_doc());
+                JButton button = openDirButtons[numDoc - 1];
+                button.setEnabled(true);
             }
         }
     }
+
+    private void abrirDirectorio(String ruta) {
+        try {
+            Desktop.getDesktop().open(new File(ruta));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
 
